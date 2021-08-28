368e0ebe05a4c7591299ffd566f4ded086dd0555
TijmenSpijk <tijmenspijk@gmail.com>
2021-08-28 23:10:44 +0200

    chapter 4.2

--- main.cpp
+++ main.cpp
@@ -4,21 +4,41 @@
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+}
+
 int main() {
 
     // Image
+    const auto aspect_ratio = 16.0/9.0;
+    const int image_width = 400;
+    const int image_heigth = static_cast<int>(image_width / aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // Camera
+
+    auto viewport_heigth = 2.0;
+    auto viewport_width = aspect_ratio * viewport_heigth;
+    auto focal_length = 1.0;
+    
+    auto origin = point3(0,0,0);
+    auto horizotal = vec3(viewport_width,0,0);
+    auto vertical = vec3(0,viewport_heigth,0);
+    auto lower_left_corner = origin - horizotal/2 - vertical/2 - vec3(0,0,focal_length);
 
     // Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_heigth << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_heigth-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_heigth-1);
+            ray r(origin, lower_left_corner + u*horizotal + v*vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
