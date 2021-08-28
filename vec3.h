622c4f18c93615a5b84538a5bc762bfb2a52a4c5
TijmenSpijk <tijmenspijk@gmail.com>
2021-08-28 22:45:37 +0200

    chapter 3.3

--- vec3.h
+++ vec3.h
@@ -9,23 +9,15 @@ using std::sqrt;
 class vec3 {
     public:
         vec3() : e{0,0,0} {}
-        vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        float x() const {return e[0];}
-        float y() const {return e[1];}
-        float z() const {return e[2];}
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
 
-        vec3 operator-() const { 
-            return vec3(-e[0], -e[1], -e[2]); 
-        }
-
-        float operator[](int i) const { 
-            return e[i]; 
-        }
-
-        float& operator[](int i) { 
-            return e[i]; 
-        }
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
 
         vec3& operator+=(const vec3 &v) {
             e[0] += v.e[0];
@@ -33,36 +25,34 @@ class vec3 {
             e[2] += v.e[2];
             return *this;
         }
-        
-        vec3& operator*=(const float t) {
+
+        vec3& operator*=(const double t) {
             e[0] *= t;
             e[1] *= t;
             e[2] *= t;
             return *this;
         }
-        
-        vec3& operator/=(const float t) {
+
+        vec3& operator/=(const double t) {
             return *this *= 1/t;
         }
 
-        float length() const {
+        double length() const {
             return sqrt(length_squared());
         }
 
-        float length_squared() const {
+        double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
-
     public:
-        float e[3];
+        double e[3];
 };
 
-// aliases for vec3
-using point3 = vec3;
-using color = vec3;
+// Type aliases for vec3
+using point3 = vec3;   // 3D point
+using color = vec3;    // RGB color
 
-#endif
 
 // vec3 Utility Functions
 
@@ -109,3 +99,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
