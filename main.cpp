3ed0b55dfe3cc9b80622de0edaf289195ec0f6d1
TijmenSpijk <tijmenspijk@gmail.com>
2021-09-02 16:27:57 +0200

    chapter 7

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "./classes/color.h"
+#include "./classes/camera.h"
 #include "./classes/hittable_list.h"
 #include "./classes/sphere.h"
 #include "./classes/rtweekend.h"
@@ -21,6 +22,7 @@ int main() {
     const auto aspect_ratio = 16.0/9.0;
     const int image_width = 400;
     const int image_heigth = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -28,15 +30,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
-
-    auto viewport_heigth = 2.0;
-    auto viewport_width = aspect_ratio * viewport_heigth;
-    auto focal_length = 1.0;
-    
-    auto origin = point3(0,0,0);
-    auto horizotal = vec3(viewport_width,0,0);
-    auto vertical = vec3(0,viewport_heigth,0);
-    auto lower_left_corner = origin - horizotal/2 - vertical/2 - vec3(0,0,focal_length);
+    camera cam;
 
     // Render
 
@@ -45,11 +39,14 @@ int main() {
     for (int j = image_heigth-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_heigth-1);
-            ray r(origin, lower_left_corner + u*horizotal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0,0,0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_heigth-1);
+                ray r = cam.get_ray(u,v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
